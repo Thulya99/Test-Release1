2d0728483fe6925125a4501d47a292384e64d6d1
Thulya Premathilake <thulyapremathilaka@gmail.com>
2024-08-29 12:30:39 +0530

    Update Node.js version to meet semantic-release requirements

---
+++ script2.js
@@ -0,0 +1,24 @@
+const { Builder, By, Key, until } = require('selenium-webdriver');
+
+(async function example() {
+  
+    let driver = await new Builder().forBrowser('chrome').build();
+    await driver.manage().window().maximize();
+
+    try {
+        await driver.get('https://platned.com/contact-us/');
+
+        let title = await driver.getTitle();
+        console.log('Page title is:', title);
+
+        // await driver.wait(until.elementLocated(By.css('h1')), 10000);
+
+        // let elementText = await driver.findElement(By.css('h1')).getText();
+        // console.log('Element text is:', elementText);
+    } finally {
+        await driver.quit(); //TS
+        console.log("new rules") //test 
+
+        //new comment line
+    }
+})();
